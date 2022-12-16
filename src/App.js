c1d8f6eb95bdb2a332d2e5233dff54c8cbd7b9cb
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-12-16 12:29:54 +0000

    change title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          art portfolio
         </a>
       </header>
     </div>
